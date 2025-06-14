21ed8727373193b3f0679db0a67cf14c6520a8f4
Khoa Lee Day <lekhoa6a7cva@gmail.com>
2025-06-14 22:23:48 +0700

    Update swagger

--- src/prescribed_regiments/prescribed_regiments.controller.ts
+++ src/prescribed_regiments/prescribed_regiments.controller.ts
@@ -9,11 +9,14 @@ import {
   Patch,
 } from '@nestjs/common';
 import { PrescribedRegimentsService } from './prescribed_regiments.service';
-import { CreatePrescribedRegimentDto, SuggestRegimentDto } from './dto/create-prescribed_regiment.dto';
+import {
+  CreatePrescribedRegimentDto,
+  SuggestRegimentDto,
+} from './dto/create-prescribed_regiment.dto';
 import { UpdatePrescribedRegimentDto } from './dto/update-prescribed_regiment.dto';
 import { PrescribedRegiment } from './schemas/prescribed_regiment.schema';
-import { ApiTags } from '@nestjs/swagger';
-import { User } from 'src/decorator/customize';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ResponseMessage, User } from 'src/decorator/customize';
 import { IUser } from 'src/users/user.interface';
 
 @ApiTags('Phác đồ cá nhân')
@@ -24,43 +27,58 @@ export class PrescribedRegimentsController {
   ) {}
 
   @Post()
+  @ApiOperation({ summary: 'Tạo Regiment cá nhân mới' })
+  @ResponseMessage('Create a new prescribedRegiments')
   async create(
     @Body() createPrescribedRegimentDto: CreatePrescribedRegimentDto,
-    @User() user: IUser
+    @User() user: IUser,
   ) {
-    return this.prescribedRegimentsService.create(createPrescribedRegimentDto, user);
+    return this.prescribedRegimentsService.create(
+      createPrescribedRegimentDto,
+      user,
+    );
   }
 
   @Get()
+  @ApiOperation({ summary: 'Lấy tất cả Regiment cá nhân' })
+  @ResponseMessage('Show all PrescribedRegiments')
   async findAll() {
     return this.prescribedRegimentsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Lấy Regiment cá nhân theo id' })
+  @ResponseMessage('Get a PrescribedRegiment by id')
   async findOne(@Param('id') id: string) {
     return this.prescribedRegimentsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update Regiment cá nhân theo id' })
+  @ResponseMessage('Update PrescribedRegiments by id')
   async update(
     @Param('id') id: string,
     @Body() updatePrescribedRegimentDto: UpdatePrescribedRegimentDto,
-    @User() user: IUser
+    @User() user: IUser,
   ) {
     return this.prescribedRegimentsService.update(
       id,
       updatePrescribedRegimentDto,
-      user
+      user,
     );
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete Regiment cá nhân theo id' })
+  @ResponseMessage('Delete PrescribedRegiments by id')
   async delete(@Param('id') id: string, @User() user: IUser) {
     return this.prescribedRegimentsService.remove(id, user);
   }
 
   @Post('suggest')
+  @ApiOperation({ summary: 'Gợi ý Regiment cá nhân theo testResults' })
+  @ResponseMessage('Suggest PrescribedRegiments by TestResults')
   async suggestRegiment(@Body() dto: SuggestRegimentDto) {
-  return this.prescribedRegimentsService.suggestRegiment(dto.testResults);
-}
+    return this.prescribedRegimentsService.suggestRegiment(dto.testResults);
+  }
 }
